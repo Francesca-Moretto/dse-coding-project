b66e6e7e6ef10f75ed560ffb50bf45cb071012b6
Francesca-Moretto <146561335+Francesca-Moretto@users.noreply.github.com>
2024-11-01 16:46:11 +0100

    better changes

--- Script 1.py
+++ Script 1.py
@@ -5,21 +5,16 @@ print('Hello!')
 #library 
 import pandas as pd
 
-#import file csv
-cities_df = pd.read_csv('C:/Users/39348/Documents/Università/Magistrale/In corso/Coding/worldcitiespop.csv')
+df_cities = pd.read_csv("worldcitiespop.csv")
+
+print(df_cities)
 
 #analyse the structure of the dataset
-print(cities_df.head)
-print(cities_df.columns)
+print(df_cities.columns)
+print(df_cities.head)
+print(df_cities.describe)
 
-#fixing the first row with the names of columns 
-cities_df = pd.read_csv('C:/Users/39348/Documents/Università/Magistrale/In corso/Coding/worldcitiespop.csv', header=None)
-print(cities_df.head())
-cities_df.columns = ['Country', 'City', 'AccentCity', 'Region', 'Population', 'Latitude', 'Longitude']
-print(cities_df.head) #still not good format 
 
-#import keeping row structure
-raw_df = pd.read_csv('C:/Users/39348/Documents/Università/Magistrale/In corso/Coding/worldcitiespop.csv', header=None)
 
 #check the number of columns in each row
 row_column_counts = raw_df.apply(lambda row: len(row.dropna()), axis=1)
